a6b97d6e947cab6920f667cff8408542b28330a3
ansarihalima <halimasadiahalimasadia00@gmail.com>
2024-05-22 04:09:34 -0700

    chapter-3 task completed

---
+++ index.js
@@ -0,0 +1,23 @@
+///////////////////////Question-1
+
+var age = 14;
+alert("I am" + " "+ age + " years old");
+
+///////////////////////Question-2
+
+// Declare and initialize the birthYear variable with your birth year
+var birthYear = 2000; 
+// Show the message in the browser
+document.write("My birth year is: " + birthYear +  "<br>");
+document.write("Data Type of my declare variable is number" +  "<br>" +  "<br>")
+
+
+///////////////////////Question-3
+// Store visitor's information in variables
+var visitorName = "Hey!Ansari";
+var productTitle = "Pent-shirt";
+var quantity = 2;
+
+// Show the message in the browser
+document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.");
+
